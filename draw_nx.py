5c4bf930804f01bdcf17dd596143bd4942d160d5
keiohigh2nd <keiohigh2nd@gmail.com>
2014-01-17 12:43:56 +0900

    parse from path list

--- draw_nx.py
+++ draw_nx.py
@@ -11,7 +11,8 @@ except:
 
 import networkx as nx
 import re
- 
+import get_compounds_from_gene2 
+
 def make_dic():
         f = open("data/compound_id_converter.txt")
         data = f.readlines()
@@ -193,20 +194,108 @@ def Find_Neighbors(G, pos, neg):
                         f1.write('\n')
 	f1.close()
 
+def Find_Neighbors2(pos, neg):
+        #Write Result
+        f1 = open('result.txt','w')
+
+        for x in pos:
+                try:
+                        tmp = get_neighbor2(x[0])
+			print tmp
+                        for t in x:
+				print "WRITE"
+                                f1.write(t.strip('\r\n'))
+                                f1.write('\t')
+                        #All neighbors
+                        for x1 in tmp:
+                                tmp_data = show_data(pos,neg,x1)
+                                for z in tmp_data:
+                                        f1.write(z.strip('\r\n'))
+                                        f1.write('\t')
+                                #Neighbors of query compound try to find related gene
+                                tmp_gene = read_gene(tmp_data[0])
+                                if tmp_gene:
+                                        f1.write(tmp_gene.strip('\r\n'))
+                                        f1.write('\t')
+                                        f1.write(gene_foldchange(tmp_gene).strip('\n'))
+                                        f1.write('\t')
+                        res_gene = read_gene(x[0])
+                        #Query Compound Related Gene
+                        if res_gene:
+                                f1.write(res_gene.strip('\r\n'))
+                                f1.write("\t")
+                                f1.write(gene_foldchange(res_gene).strip('\n'))
+                        f1.write('\n')
+                except:
+                        #print "not found"
+                        #f1.write(x[1])
+                        f1.write('\n')
+
+        for y in neg:
+                try:
+                        tmp = get_neighbor2(y[0])
+                        for t in y:
+				print "write"
+                                f1.write(t.strip('\r\n'))
+                                f1.write('\t')
+                        for y1 in tmp:
+                                tmp_data = show_data(pos,neg,y1)
+                                for z in tmp_data:
+                                        f1.write(z.strip('\r\n'))
+                                        f1.write('\t')
+                                #Neighbors of query compound try to find related gene
+                                tmp_gene = read_gene(tmp_data[0])
+                                if tmp_gene:
+                                        f1.write(tmp_gene.strip('\n'))
+                                        f1.write('\t')
+                                        f1.write(gene_foldchange(tmp_gene).strip('\n'))
+                                        f1.write('\t')
+                        res_gene = read_gene(y[0])
+                        #Query Compound Related Gene
+                        if res_gene:
+                                f1.write(res_gene.strip('\r\n'))
+                                f1.write('\t')
+                                f1.write(gene_foldchange(res_gene).strip('\n'))
+                        f1.write('\n')
+                except:
+                        #print "not found"
+                        #f1.write(y[1])
+                        f1.write('\n')
+        f1.close()
+
+
+
+
+def get_neighbor2(id):
+	f = open("data/hsa_list.txt")
+	line = f.readlines()
+	res = []
+	for x in line:
+		tmp = get_compounds_from_gene2.get_compound(x[5:13])
+		for y in tmp:
+			#If id is in the passway
+			if id == y:
+				res.append(tmp)
+				break
+	return res
+
 if __name__ == "__main__":
 
 	#Read Compound
 	pos = read_data("data/metabo_data_1126_pos_ave.csv")
         neg = read_data("data/metabo_data_1126_neg_ave.csv")
 	
-	G = Draw_Graph("data/node_neo.txt")
+	#G = Draw_Graph("data/node_neo.txt")
 
 	#Test
 	"""
 	tmp = nx.all_neighbors(G,"C00311")
 	for x in tmp:
 		print x
+
+        tmp = get_neighbor2("C00311")
+        print tmp
 	"""
 
-	Find_Neighbors(G,pos,neg)
+	Find_Neighbors2(pos,neg)
 
---
+++ fornohuo.sh
@@ -0,0 +1,1 @@
+python2.7 draw_nx.py
